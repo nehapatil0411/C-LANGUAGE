79a279401cf3b406536d790c946c58f29af02f68
nehapatil0411 <8561nehapatil@gmail.com>
2024-06-24 11:01:08 +0500

    Add files via upload

---
+++ operator.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+	int x,y; 
+	printf("enter value of x:");
+	scanf("%d",&x);
+	printf("enter value of y:");
+	scanf("%d",&y);
+	printf("(x+y)2=%d",(x*x)+(2*x*y)+(y*y));
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ operator3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+	int x,y; 
+	printf("enter value of x:");
+	scanf("%d",&x);
+	printf("enter value of y:");
+	scanf("%d",&y);
+	printf("(x+y)3=%d",(x*x*x)+(3*x*y)+(x*y)+(y*y*y));
+	
+	
+	
+	
+	
+	
+	
+}
