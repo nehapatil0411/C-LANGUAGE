99221407e9b0bed1e6e973aebc6ff3abd477dc20
NEHA PATIL <8561nehapatil@gmail.com>
2024-07-19 11:33:16 +0500

    Add files via upload

---
+++ que2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+	int n;
+	int a=1;
+	
+	printf("enter n :");
+	scanf("%d",&n);
+	while(a<=n)
+	{
+		printf("%d ",a);
+	a++;
+
+	}
+
+}
---
+++ que5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	
+	int a=10;
+	
+	while(a>=1)
+	{
+		printf("%d ",a);
+		a--;
+	}
+}
