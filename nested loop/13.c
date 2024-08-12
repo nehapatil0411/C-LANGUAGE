7f61d89542fd3b31e477a2cb2a58b4e25c0dd134
NEHA PATIL <8561nehapatil@gmail.com>
2024-08-12 11:29:34 +0530

    Add files via upload

---
+++ nested loop/13.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	
+	int i,j;
+	char k='A';
+	for(i=1; i<=5; i++)
+	{
+		for(j=1; j<=i; j++)
+		{
+			printf("%c ",k);
+			k++;
+		}
+		printf("\n");
+	}
+}
